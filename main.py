1572fe328821c9c26797f51066ee1b636c930225
Anton <Podskrebalin93@mail.ru>
2026-05-05 16:23:51 +0300

    added average and empty users functions

--- main.py
+++ main.py
@@ -4,3 +4,19 @@ users = {
     "Maria": 200,
     "Stas": 1000,
 }
+
+def get_average_balance(users):
+    return sum(users.values()) / len(users)
+
+
+print(get_average_balance(users))
+
+
+def empty_user(users):
+    empty_user = []
+    for name, balance in users.items():
+        if balance == 0:
+            empty_user.append(name)
+    return empty_user
+
+print(empty_user(users))
