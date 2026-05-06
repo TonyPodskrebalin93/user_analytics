565c3029f7f5e9e48624d334b81623401a004f35
Anton <Podskrebalin93@mail.ru>
2026-05-06 20:24:07 +0300

    improved analyze_users logic

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ users = {
     "Anton": 1000,
     "Ivan": 2500,
     "Maria": 200,
-    "Stas": 1000,
+    "Stas": 0,
 }
+
 
 def get_average_balance(users):
     return sum(users.values()) / len(users)
@@ -12,11 +13,51 @@ def get_average_balance(users):
 print(get_average_balance(users))
 
 
-def empty_user(users):
+def get_empty_user(users):
     empty_user = []
     for name, balance in users.items():
         if balance == 0:
             empty_user.append(name)
     return empty_user
 
-print(empty_user(users))
+
+print(get_empty_user(users))
+
+
+def poor_users(users,threshold):
+    poor_users = []
+    for name, balance in users.items():
+        if balance < threshold:
+            poor_users.append(name)
+    return poor_users
+
+
+print(poor_users(users, 1000))
+
+
+def analyze_users(users):
+    result = {
+        "total_users": 0,
+        "average_balance": 0,
+        "richest_users": None,
+        "poor_users": [],
+    }
+
+    richest_balance = None
+    richest_name = None
+
+    for name, balance in users.items():
+        if richest_balance is None or balance > richest_balance:
+            richest_balance = balance
+            richest_name = name
+
+    result = {
+        "total_users": len(users),
+        "average_balance": get_average_balance(users),
+        "richest_user": richest_name,
+        "poor_users": poor_users(users, 1000),
+    }
+    return result
+
+
+print(analyze_users(users))
